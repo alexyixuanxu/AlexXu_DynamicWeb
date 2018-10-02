42c00ad14b41065d4efaf74f01d1f0b70028b8c4
Alex Yixuan Xu <yx1137@nyu.edu>
2018-10-02 14:39:42 -0400

    hide keys

--- 10-2/inClass/config.js
+++ 10-2/inClass/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key:         'tpLiLP5TaAtmx4IL2lG6hBQh3',
-  consumer_secret:      'RtSZXQ5ATjCQddTOvmKD6paCL4RwyHyhECfUoeYMSf4OVFbxhQ',
-  access_token:         '992461848443326465-fkzGzDn1Zxq0Tgx8ynXEvpil4oLhjXl',
-  access_token_secret:  'Y1WsAkerOqVtDGwWXv8T7aza8xD0eeJfCRf0rtWKzkdDh',
+  consumer_key:         '#',
+  consumer_secret:      '#',
+  access_token:         '#',
+  access_token_secret:  '#',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
