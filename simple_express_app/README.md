6f403dd882896e3c6be57950fa811ab3a61ac8b4
Alex Yixuan Xu <yx1137@nyu.edu>
2018-11-04 22:28:28 -0500

    added Assignment 6

---
+++ simple_express_app/README.md
@@ -0,0 +1,14 @@
+# Assignment 6: Simple Express App
+This web app is called "Shiba or Cat?" in which the user select shiba or cat and input a number (1-100) to see that number of images of the selected animal. 
+
+## Set Up the App
+This app uses Node.js, body parser module, request module, EJS templating, and Express framework.
+1. Download the zip file or clone this respository to your desired directory on your computer.
+2. Download [node.js](https://nodejs.org/en/download/)
+3. In your terminal, navigate to your directory that contain the respository and type in `npm install express`, `npm install body-parser`, `npm install request`, `npm install ejs` to install the packages.
+
+## Run & Test the App
+In your terminal, navigate to the directory containing the files, and type in `node app.js` to run the app. It should start a server at `localhost/3000`.
+
+________________________
+By Alex Yixuan Xu
---
+++ simple_express_app/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const ejs = require('ejs');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res){
+	res.render('index', {userInput: null, imgLinksArr: null, error: null});
+});
+
+app.post('/', function (req, res){
+	let pet = req.body.pet;
+	let num = req.body.num;
+	console.log(req.body);
+	// check whether a selection is made
+	if ("pet" in req.body){
+		// check whether input number is positive integer>=1 and <=100
+		if (Number.isInteger(Number(num)) && Number(num)>=1 && Number(num)<=100){
+			let url = `http://shibe.online/api/${pet}?count=${num}`;
+			console.log(url);
+			request(url, function(err, response, body){
+				// if error getting data from api
+				if(err){
+					res.render('index', {userInput: null, imgLinksArr: null, error: 'Error getting images...please try again...'});
+				}
+				else{
+					let dataArr = JSON.parse(body);
+					console.log(dataArr);
+					res.render('index', {userInput: req.body, imgLinksArr: dataArr, error: null});
+				}
+			});
+		}
+		// gives error message is not valid number
+		else{
+			res.render('index', {userInput: null, imgLinksArr: null, error: 'Not a valid number (1-100)!'});
+		}
+	}
+	// if user did not make a selection
+	else{
+		res.render('index', {userInput: null, imgLinksArr: null, error: 'You have to select something!'});
+	}
+
+});
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000')
+})
